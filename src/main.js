f8fed3f53fc0050f9e75b1de77d821cf3beae861
programmerAju <programmerhead7@gmail.com>
2024-01-16 23:21:39 +0530

    deities part finished

--- src/main.js
+++ src/main.js
@@ -5,3 +5,4 @@ import "../styles/components/header.css";
 import "../styles/components/introduction.css";
 import "../styles/components/events.css";
 import "../styles/components/deities.css";
+import "../styles/components/contacts.css";
